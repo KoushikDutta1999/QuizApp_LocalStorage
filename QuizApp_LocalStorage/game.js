266700c86eee6e53d7141d6fa34747e1403072e8
Koushik Dutta <koushikdutta.bca2019@nsec.ac.in>
2021-10-25 13:59:42 +0530

    Update game.js

--- QuizApp_LocalStorage/game.js
+++ QuizApp_LocalStorage/game.js
@@ -55,7 +55,7 @@ getNewQuestions = () => {
 
     if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
         localStorage.setItem("mostRecentScore",score);
-        return window.location.assign("/end.html");
+        return window.location.assign("end.html");
     }
     
     questionCounter++;
